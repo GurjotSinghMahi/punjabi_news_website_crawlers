e34f74548910c66566fedec01c6eb3d69dd78a4d
GURJOT SINGH MAHI <gurjotmahi28@gmail.com>
2020-07-12 22:57:07 +0530

    dynamic website changes adjusted

--- JAGBANI_CRAWLER.py
+++ JAGBANI_CRAWLER.py
@@ -10,6 +10,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import quote
 from urllib.request import urlopen, Request, URLError
 
+
 '''
 initialize file name for news data management
 
@@ -39,13 +40,13 @@ except socket.timeout:
 except URLError:
     pass
 
-workbook = ''
+
 def make_directory(FolderName):
-    path = r'F:\jagbani Corpus\\' + FolderName
+    path = r'F:\jagbani Corpus\\' + str(FolderName)
     if not os.path.exists(path):
         os.makedirs(path)
 def create_excel_sheet(FolderName):
-    FactSheet = r'F:\jagbani Corpus\\' + FolderName + '\\' + FolderName + '_STATS.xlsx'
+    FactSheet = r'F:\jagbani Corpus\\' + str(FolderName) + '\\' + str(FolderName) + '_STATS.xlsx'
     global workbook
     workbook = xlsxwriter.Workbook(FactSheet)
     global worksheet1
@@ -57,25 +58,12 @@ def create_excel_sheet(FolderName):
     worksheet1.write(0, 4, "Date")
     worksheet1.write(0, 5, "Year")
 
-def initialize():
-    if html is None:
-        print("URL is not found")
-    else:
-        '''
-        This parts extracts all the link for in jagbani website pages    
-        '''
-        bsObj = BeautifulSoup(html, "html.parser")
-        front_url = "http://jagbani.punjabkesari.in"
-        div = bsObj.findAll('div', attrs={'class': 'kjpage'})
-        for page_no in div:
-            links = page_no.find_all('a')
-            for link in links:
-                jagbani_pages.append(front_url+link['href'])
 
-file_no = 0
+workbook = ''
+file_number = 0
 row = 1
 col = 0
-def text_extraction(url):
+def text_extraction(url, genre, filenumber):
     '''
     :param url: The page url from which we want to extract the text
     :return:
@@ -87,68 +75,79 @@ def text_extraction(url):
     allurls = []
     html = urlopen(url)
     parser = BeautifulSoup(html, "html.parser")
-    spans = parser.findAll('span', attrs={'class': lambda class_: class_ in ("story", "midle")})
-    for span in spans:
-        #print(span.get_text())
-        links = span.find_all('a')
-        for link in links[:1]:
-            allurls.append(link['href'])
-        for url in allurls:
-            filename = "text_" + str(file_no) + ".txt"
-            print(url)
-            print(filename)
-            print(col)
-            print(row)
-            worksheet1.write(row, col, filename)
-            col += 1
-            worksheet1.write(row, col, span.get_text())
-            col += 1
-            regex = re.compile(r'.*//.*\.in/([^\/]*)/.*')
-            news_genre = regex.sub(lambda m: m.group(1), url)
-            worksheet1.write(row, col, news_genre)
-            col += 1
-            req = urllib.request.Request(url)
-            try:
-                req.selector.encode('ascii')
-            except UnicodeEncodeError:
-                req.selector = quote(req.selector)
-            response = urllib.request.urlopen(req, timeout=30)
-            news_url = response.read().decode('utf-8')
-            particular_news_parser = BeautifulSoup(news_url, "html.parser")
-            for time in particular_news_parser.findAll('div', attrs={'class': 'time2'}):
-                worksheet1.write(row, col, time.text)
-                col += 1
-            div = particular_news_parser.findAll('div', attrs={'class': 'desc'})
-            '''
-            Location of the filename, where you want to save your data_files
-            '''
-            file = "D:\\data_files\\" + filename
-            with open(file, 'w', encoding='utf8') as f:
-                for x in div:
-                    f.write(x.find('p').text)
-            file_no += 1
-            col = 0
-            row += 1
-            allurls = []
-
-def get_page_links(url, genre_name, start_num, end_num):
-    website_url = "https://punjabi.jagran.com/"
-    jagran_page_link = website_url + genre_name +"-news-punjabi-page"
-    for i in range(start_num, end_num):
-        link = jagran_page_link + str(i) + ".html"
-        get_page_title_and_link(link, genre_name)
-        print(link)
+    # TITLE DATA
+    title = parser.find('div', attrs={'id': 'ContentPlaceHolder1_dv_headline'}).h1.get_text()
+    date_text = parser.find('div', attrs={'class': 'dt_time'}).span.get_text()
+    #print(title, date_text)
+    pattern = r"([0-9]+) ([a-zA-Z]+), ([0-9]+)"
+    date = re.search(pattern, date_text).group(1)
+    month = re.search(pattern, date_text).group(2)
+    year = re.search(pattern, date_text).group(3)
+    print("Date: ", date, " Month: ", month, " Year: ", year)
+    tags = ['center', 'strong', 'a','ul','div']
+    para = parser.find('div', attrs={'id': 'ContentPlaceHolder1_dv_main_news_detail'})
+    for t in tags:
+        [s.extract() for s in para(t)]
+    para_text = ''
+    for el in para.find_all():
+        para_text += ''.join(el.text)
+    result = re.sub(r"ਇਹ ਵੀ ਪੜ੍ਹੋ:", "", para_text, 0, re.MULTILINE)
+    print(result)
+    filename = "text_" + str(filenumber) + ".txt"
+    path = r'F:\jagbani Corpus\\' + genre + '\\' + filename
+    global row, col
+    global worksheet1
+    worksheet1.write(row, col, filename)
+    col += 1
+    worksheet1.write(row, col, title)
+    col += 1
+    worksheet1.write(row, col, genre)
+    col += 1
+    worksheet1.write(row, col, month)
+    col += 1
+    worksheet1.write(row, col, int(date))
+    col += 1
+    worksheet1.write(row, col, int(year))
+    col += 1
+    print(title)
+    with open(path, 'w', encoding='utf8') as f:
+        f.write(result)
+    row += 1
+    col = 0
 
 
+
+def get_page_links(url, genre_name):
+    global file_number
+    jagran_page_link = url + genre_name
+    genre_page = Request(jagran_page_link, headers={'User-Agent': 'Mozilla/5.0'})
+    try:
+        genre_page.selector.encode('ascii')
+    except UnicodeEncodeError:
+        genre_page.selector = quote(genre_page.selector)
+    try:
+        page_response = urllib.request.urlopen(genre_page, timeout=30)
+        page = page_response.read().decode('utf-8')
+        parser = BeautifulSoup(page, "html.parser")
+        for ul in parser.findAll('ul', attrs={'id': 'ContentPlaceHolder1_dv_section_middle'}):
+            for li in ul.find_all('h3'):
+                a = li.find('a')
+                print(a['href'])
+                text_extraction(a['href'], genre_name, file_number)
+                file_number += 1
+    except socket.timeout:
+        pass
+    except URLError:
+        pass
+    except socket.error:
+        pass
 
 def main():
-    make_directory("sports")
-    create_excel_sheet("sports")
-    #initialize()
-    get_page_links(link, genre, start, end)
-    for url in jagbani_pages:
-        print("URL: ", url)
-        text_extraction(url)
+    link = "https://jagbani.punjabkesari.in/"
+    genre = "punjab"
+    make_directory(genre)
+    create_excel_sheet(genre)
+    get_page_links(link, genre)
     workbook.close()
 
 if __name__ == '__main__':
